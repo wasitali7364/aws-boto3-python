5a70ea3ab45a698de1d130ddd702aaeafbff5150
Md Wasit Ali Nayyar <65351165+wasitali7364@users.noreply.github.com>
2023-02-27 17:16:26 +0530

    Create download_files_aws_boto3.py

---
+++ download_files_aws_boto3.py
@@ -0,0 +1,18 @@
+import boto3
+from pathlib import Path
+
+data_folder = Path.cwd() / 'Data'
+data_folder.mkdir(parents=True, exist_ok=True)
+
+# create a session
+s3 = boto3.resource(service_name = 's3')
+
+for obj in s3.Bucket('c2foupload-inprod').objects.filter(Prefix='anime/latest/merged/2023'):
+    if '/74/' in obj.key and 'merged_award' in obj.key:
+        date = obj.key[27:37]
+        file = date + '-' + obj.key[-24:]
+        file_name = data_folder.joinpath(file)
+        s3_key = obj.key
+        s3.Bucket('c2foupload-inprod').download_file(Key = s3_key, Filename = str(file_name))
+
+print("Download Successfull")
